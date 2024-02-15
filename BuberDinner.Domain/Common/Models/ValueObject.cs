99d8303deea20b64056631a7c74184c8dbb1a9cc
Alexander Chelyadinov <artma.gronsky@gmail.com>
2024-02-15 22:13:56 +0300

    Add aggregateRoot, entity and valueObject

---
+++ BuberDinner.Domain/Common/Models/ValueObject.cs
@@ -0,0 +1,53 @@
+namespace BuberDinner.Domain.Common.Models;
+
+public abstract class ValueObject: IEquatable<ValueObject>
+{
+    public abstract IEnumerable<object> GetEqualityComponents();
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((ValueObject)obj);
+    }
+
+    public static bool operator ==(ValueObject left, ValueObject right)
+    {
+        return Equals(left, right);
+    }
+
+    public static bool operator !=(ValueObject left, ValueObject right)
+    {
+        return !Equals(left, right);
+    }
+
+    public override int GetHashCode()
+    {
+        return GetEqualityComponents()
+            .Select(x => x?.GetHashCode() ?? 0)
+            .Aggregate((x, y) => x ^ y);
+    }
+    public bool Equals(ValueObject? other)
+    {
+        return Equals((object?)other);
+    }
+}
+
+public class Price : ValueObject
+{
+    public decimal Amount { get; private set; }
+    public string Currency { get; private set; }
+
+    public Price(decimal amount, string currency)
+    {
+        Amount = amount;
+        Currency = currency;
+    }
+
+    public override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Amount;
+        yield return Currency;
+    }
+}
